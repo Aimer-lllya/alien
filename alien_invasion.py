fb9f9626a87e5c8bb85f832561327d1b71313d68
Aimer-lllya <986104289@qq.com>
2020-08-05 17:02:05 +0800

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,7 @@ def run_game():
 
     #开始游戏主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullet(ai_settings, screen, ship, aliens, bullets)
--- game_functions.py
+++ game_functions.py
@@ -23,7 +23,7 @@ def check_keyup_events(event,ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """监视键盘和鼠标事件"""
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -32,6 +32,29 @@ def check_events(ai_settings, screen, ship, bullets):
                 check_keydown_events(event,ai_settings, screen, ship, bullets)
             elif event.type == pygame.KEYUP:
                 check_keyup_events(event,ship)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                check_play_button(ai_settings, screen, stats, play_button, ship, 
+                aliens, bullets, mouse_x, mouse_y)
+
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
+    """在玩家单击Play按钮时开始游戏"""
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        # 隐藏光标
+        pygame.mouse.set_visible(False)
+
+        # 重置游戏统计信息
+        stats.reset_stats()
+        stats.game_active = True
+
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
+
+        # 创建一群外星人，并让飞船居中
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
                 
 def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
     #重新绘制屏幕
@@ -143,6 +166,7 @@ def ship_hit(ai_settings,stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings,stats, screen, ship, aliens, bullets):
     #检查是否有外星人到达底部
