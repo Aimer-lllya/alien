3ab44110cee9868dabc4aaee88eb3d6cfbc8f597
Aimer-lllya <986104289@qq.com>
2020-08-05 17:14:15 +0800

    update

--- game_functions.py
+++ game_functions.py
@@ -41,6 +41,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """在玩家单击Play按钮时开始游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
         # 隐藏光标
         pygame.mouse.set_visible(False)
 
@@ -90,10 +92,10 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        #删除现有子弹并新建一群外星人
+        #删除现有子弹，加快游戏节奏，并新建一群外星人
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
-
 
 def fire_bullet(ai_settings,screen,ship,bullets):
     """如果没有达到子弹上限就发射一颗子弹"""
